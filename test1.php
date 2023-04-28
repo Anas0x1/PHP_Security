6a0bcb57e4c0ce875acd6fdce61578fa48ee03fe
0xanas <anasebrahim514@gmail.com>
2023-04-28 19:11:20 +0300

    Delete test1.php

--- test1.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form id="xss">
-            <?php                
-                echo '<h1>Hello ' . $_GET['name'] . '</h1>';  
-            ?>
-        </form>
-    </body>
-</html>
