d6f985e19b6e776c12e7a44c11c949c45ab6d62e
Anas Ibrahim <78263620+Anas0x1@users.noreply.github.com>
2024-11-12 04:02:17 +0200

    Update dom.php

--- XSS/DOM XSS/dom.php
+++ XSS/DOM XSS/dom.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html>
 <head>
-	DOM XSS vulnerability example
-	<title>DOM XSS</title>
+	<title>DOM XSS Vulnerability Example</title>
 </head>
 <body>
 <form method="get">
 	<input type="text" name="name">
 	<input type="submit">
-	<?php 
-		if (isset($_GET['name']))
-		{
-			$name = $_GET['name'];
-			echo '<script>document.write("<p>Hello, ' . $name . '!</p>");</script>';
-		}
-	?>
 </form>
+
+<script>
+	// Get the 'name' parameter from the URL
+	const params = new URLSearchParams(window.location.search);
+	const name = params.get('name');
+
+	// Directly write unescaped user input to the page, making it vulnerable to XSS
+	if (name) {
+		document.write("<p>Hello, " + name + "!</p>");
+	}
+</script>
 </body>
 </html>
