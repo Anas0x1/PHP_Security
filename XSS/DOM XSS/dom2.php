be21e48a729b936b271b3b33588e9c1606e4e71f
Anas Ibrahim <78263620+Anas0x1@users.noreply.github.com>
2024-11-12 04:03:15 +0200

    Update dom2.php

--- XSS/DOM XSS/dom2.php
+++ XSS/DOM XSS/dom2.php
@@ -6,20 +6,22 @@
 <body>
 	<h1>DOM XSS Example</h1>
 
-	<?php
-		if (isset($_GET['name'])) {
-			$name = $_GET['name'];
-			echo '<script>
-				var greeting = "<p>Hello, " + ' . $name . ' + "!</p>";
-				document.getElementById("greeting").innerHTML = greeting;
-			</script>';
-		}
-	?>
-
 	<p id="greeting">Enter your name:</p>
-	<form>
+	<form method="get">
 		<input type="text" name="name">
 		<input type="submit" value="Submit">
 	</form>
+
+	<script>
+		// Get the 'name' parameter from the URL
+		const params = new URLSearchParams(window.location.search);
+		const name = params.get('name');
+
+		// Directly use unescaped user input in the DOM, making it vulnerable to XSS
+		if (name) {
+			const greeting = "<p>Hello, " + name + "!</p>";
+			document.getElementById("greeting").innerHTML = greeting;
+		}
+	</script>
 </body>
 </html>
